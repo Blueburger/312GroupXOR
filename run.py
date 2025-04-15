727c11cd7cdd5f939a504e446c6989d59e508039
Jonah Thundat <102984553+Blueburger@users.noreply.github.com>
2025-04-15 15:59:17 -0400

    fixed error causing script option 1 to not work properly

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ def log_exception(e):
 
 if __name__ == "__main__":
     print("[SUCCESS] Launching Flask app on http://127.0.0.1:8080")
-    socketio.run(app, host="127.0.0.1", port=8080)
+    socketio.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True)
